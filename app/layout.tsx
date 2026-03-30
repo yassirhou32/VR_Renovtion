3793b4d6fd68767c41a67e84308b9b073ee36963
yassirhou34 <houariyassir8@gmail.com>
2026-03-30 14:47:20 +0100

    jjjjjjjjj84

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,21 +34,8 @@ export const metadata: Metadata = {
     images: ['/images/DSC08771.jpg'],
   },
   icons: {
-    icon: [
-      {
-        url: '/icon-light-32x32.png',
-        media: '(prefers-color-scheme: light)',
-      },
-      {
-        url: '/icon-dark-32x32.png',
-        media: '(prefers-color-scheme: dark)',
-      },
-      {
-        url: '/icon.svg',
-        type: 'image/svg+xml',
-      },
-    ],
-    apple: '/apple-icon.png',
+    icon: '/images/logo.png',
+    apple: '/images/logo.png',
   },
 }
 
